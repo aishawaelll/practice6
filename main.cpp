913253e416794312e9060aeccccdf5e732da64ad
Aisha <aishawaelll@aucegypt.edu>
2021-03-18 19:57:35 +0200

    prepared main source file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Person.h"
+
+int main()
+{
+	Person p1;
+	p1.setName("Aisha");
+	p1.setAge(18);
+	cout<<"Person's name: "<<p1.getName()<<". "<<p1.getName()<<"'s age: "<<p1.getAge()<<endl;
+}
+
